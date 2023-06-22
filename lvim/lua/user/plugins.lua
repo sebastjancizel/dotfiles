1f77e5a56bc3c1048c919e3b216d16a8221ed5cf
Sebastjan Cizel <sebastjancizel@gmail.com>
2023-06-22 18:49:42 +0100

    Configure harpoon

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -24,4 +24,26 @@ lvim.plugins = {
       require("spectre").setup()
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      require('harpoon')
+
+      lvim.builtin.which_key.mappings["H"] = {
+        name = "Harpoon",
+        a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add Mark" },
+        t = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle Quick Menu" },
+        n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "Navigate Next" },
+        p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Navigate Previous" },
+        f = {
+          name = "Navigate to File",
+          ["1"] = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "File 1" },
+          ["2"] = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "File 2" },
+          ["3"] = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "File 3" },
+          ["4"] = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "File 4" },
+          ["5"] = { "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", "File 5" },
+        },
+      }
+    end,
+  },
 }
