195c53df63ea731d28db5268be6205a7856dbf9c
Sebastjan Cizel <sebastjancizel@gmail.com>
2023-07-01 23:54:41 +0100

    Squashed commit of the following:
    commit 2cfaa2949ddab6d46b82a8f2d832c4f035f29438
    Author: Sebastjan Cizel <sebastjancizel@gmail.com>
    Date:   Sat Jul 1 23:48:35 2023 +0100
    
        Add dialog
    
    commit 5580268dfbae3874c51a6cd9cb6124cedaf6e196
    Author: Sebastjan Cizel <sebastjancizel@gmail.com>
    Date:   Sat Jul 1 23:46:04 2023 +0100
    
        Install neovim by default
    
    commit 3a295c2dd29db23815c494fd2fc7e09d993abda6
    Merge: f34de68 1f77e5a
    Author: Sebastjan Cizel <sebastjancizel@gmail.com>
    Date:   Sat Jul 1 23:44:46 2023 +0100
    
        Merge branch 'main' into dev
    
    commit f34de68e8f76a5a9efb0dd6d14fe5bdfa422c4e8
    Author: Sebastjan Cizel <sebastjancizel@gmail.com>
    Date:   Wed Jun 14 23:41:59 2023 +0100
    
        Adds lvim install script

--- install.sh
+++ install.sh
@@ -11,36 +11,45 @@ log "Starting installation..."
 HOME_DIR=$HOME
 
 # Install basic utilities
-log "[1/6] Installing basic utilities..."
+log "[1/7] Installing basic utilities..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get update >/dev/null
-    sudo apt-get install -y git curl vim tmux sudo tree fd-find ripgrep silversearcher-ag bat zsh >/dev/null
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get install -yq dialog git curl vim tmux sudo tree fd-find ripgrep silversearcher-ag bat zsh >/dev/null
     # Create a symlink to make bat accessible with the bat command
     sudo ln -s /usr/bin/batcat /usr/bin/bat >/dev/null
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install git curl vim tmux tree fd ripgrep the_silver_searcher bat zsh >/dev/null
 fi
 
+# Install neovim
+log "[2/7] Installing neovim..."
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install -y neovim >/dev/null
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install neovim >/dev/null
+fi
+
 # Install Oh My Zsh
-log "[2/6] Installing Oh My Zsh..."
+log "[3/7] Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null
 
 # Set ZSH environment variable
 ZSH="$HOME_DIR/.oh-my-zsh"
 
 # Create symlinks for .zshrc and .tmux.conf in the home directory (assuming they are in the same directory as this script)
-log "[3/6] Creating symlinks..."
-ln -sf "$(pwd)/.zshrc" $HOME_DIR/.zshrc
-ln -sf "$(pwd)/.tmux.conf" $HOME_DIR/.tmux.conf
-ln -sf "$(pwd)/.p10k.zsh" $HOME_DIR/.p10k.zsh
+log "[4/7] Creating symlinks..."
+ln -sf "$(pwd)/.zshrc" "$HOME_DIR/.zshrc"
+ln -sf "$(pwd)/.tmux.conf" "$HOME_DIR/.tmux.conf"
+ln -sf "$(pwd)/.p10k.zsh" "$HOME_DIR/.p10k.zsh"
 
 # Install fzf
-log "[4/6] Installing fzf..."
+log "[5/7] Installing fzf..."
 git clone --depth 1 --quiet https://github.com/junegunn/fzf.git $HOME_DIR/.fzf
 $HOME_DIR/.fzf/install --all >/dev/null
 
 # Install zsh plugins
-log "[5/6] Installing zsh plugins..."
+log "[6/7] Installing zsh plugins..."
 log "=>Cloning powerlevel10k"
 git clone --depth=1 --quiet https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$ZSH/custom}/themes/powerlevel10k
 log "=>Cloning zsh-autosuggestions"
@@ -52,4 +61,5 @@ git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH
 log "=>Cloning fzf-tab"
 git clone --quiet https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-$ZSH/custom}/plugins/fzf-tab
 
-log "[6/6] Installation complete"
+log "[7/7] Installation complete"
+
