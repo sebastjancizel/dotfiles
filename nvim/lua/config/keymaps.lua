362ca517b981fd4ee3530f66456e82a495188fc2
Sebastjan Cizel <sebastjancizel@gmail.com>
2024-03-01 22:31:33 +0000

    [feat] lazyvim
    # Conflicts:
    #	lvim_install.sh

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+map("n", "<leader>bj", "<cmd>BufferLinePick<CR>", { noremap = true, silent = true, desc = "Jump to buffer" })
+map("n", "<leader>bc", "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close buffer" })
