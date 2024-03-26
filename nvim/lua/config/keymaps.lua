d374eb33aa531c99c977f24ba71eb1f8c4d6502d
Sebastjan Cizel <sebastjancizel@gmail.com>
2024-03-26 20:43:00 +0000

    Update nvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,9 @@ map("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 map("n", "<leader>bj", "<cmd>BufferLinePick<CR>", { noremap = true, silent = true, desc = "Jump to buffer" })
 map("n", "<leader>bc", "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close buffer" })
+map(
+  "n",
+  "<leader>W",
+  "<cmd>noautocmd w<CR>",
+  { noremap = true, silent = true, desc = "Write without triggering autocommands" }
+)
